03d0c30e5a974450a3c716fd38bef3c12c879ad6
Marco Coimbra <marcocoimbra01@gmail.com>
2019-03-21 21:00:10 -0300

    xnor_matmul

--- index.py
+++ index.py
@@ -1,24 +1,57 @@
 import numpy as np
 
-size = (10,)
+size = (2,3)
 
 def to_bipolar(booleanArray):
 	result = booleanArray * 1
-	result[result == 0] = -1
+	result[result < 1] = -1
 
 	return result
 
 def xnor(a, b):
 	return np.logical_not(np.bitwise_xor(a, b))
 
+def xnor_matmul(a, b):
+	result = []
+
+	for i in range(a.shape[0]):
+		aux = []
+
+		for j in range(b.shape[1]):
+			k = xnor(a[i, :], b[:, j])
+			dot = np.count_nonzero(k) - k[k < 1].shape[0]
+
+			aux.append(dot)
+
+		result.append(aux)
+
+	return np.array(result)
+
 x1 = np.random.choice(a=[False, True], size=size)
-x2 = np.random.choice(a=[False, True], size=size)
+
+def Dense(input_size, num_units):
+	params = {
+		'weights': np.random.choice(a=[False, True], size=(input_size, num_units)),
+		'bias': np.random.choice(a=[False, True], size=(num_units,))
+	}
+
+	def forward(x): xnor(x, params['weights'])
+
+	params['forward'] = forward
+
+	return params
+
+l1 = Dense(3, 10)
+
+w = l1['weights']
 
 x1_bp = to_bipolar(x1)
-x2_bp = to_bipolar(x2)
+w_bp = to_bipolar(w)
 
 print(x1_bp)
-print(x2_bp)
-print(x1_bp * x2_bp)
-
-print(xnor(x1_bp, x2_bp))
+print('')
+print(w_bp)
+print('')
+print(np.matmul(x1_bp, w_bp))
+print('')
+print(xnor_matmul(x1, w))
